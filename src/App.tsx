71dc2b1d3d3891650cedaef245030e87e2726dd6
Amr Mohamed Ali <amr.rashidy2020@gmail.com>
2026-03-05 00:03:46 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import ProductList from "./pages/productList/ProductList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart";
-import Footer from "./components/layout/Footer";
-import Navbar from "./components/layout/Navbar";
 
-import CheckoutPage1 from "./pages/CheckoutPage1";
-import CheckoutPage2 from "./pages/CheckoutPage2";
-import CheckoutPage3 from "./pages/CheckoutPage3";
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Cart />} />
-          <Route path="/shop" element={<ProductList />} />
-          <Route path="/checkout1" element={<CheckoutPage1 />} />
-          <Route path="/checkout2" element={<CheckoutPage2 />} />
-          <Route path="/checkout3" element={<CheckoutPage3 />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Cart />} />
+        {/* <Route path="/checkout" element={<Checkout />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 };
 
