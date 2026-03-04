a41ba78a93bc1d47ab19470c5474e008005c06a3
salma <salma.abdelhares.2005@gmail.com@example.com>
2026-03-04 23:10:08 +0200

    login-task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import Autho from '../src/components/autho/Autho'
+import Account from '../src/components/account/Account'
+import {BrowserRouter,Routes,Route} from'react-router-dom'
+import Choose from '../src/components/choose/Choose'
+import Recovery from '../src/components/recovery/Recovery'
 const App = () => {
-  return <div>App</div>;
+  return(
+  <BrowserRouter>
+
+
+   
+    <Routes>
+      <Route path='/' element={<Autho/>}/>
+      <Route path='/account' element={<Account/>}/>
+
+     <Route path='/Choose' element={<Choose/>}/>
+ 
+
+<Route path='/Recovery' element={<Recovery/>}/>
+
+      </Routes>
+   
+
+</BrowserRouter>
+
+  )
 };
 
 export default App;
