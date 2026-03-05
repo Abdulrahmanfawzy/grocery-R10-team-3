008e574891240f6bc515aa001db19a15ef5a41af
Yousef sheha <sheha8757@gmail.com>
2026-03-05 21:27:31 +0200

    last

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ import CheckoutPage1 from "./pages/CheckoutPage1";
 import CheckoutPage2 from "./pages/CheckoutPage2";
 import CheckoutPage3 from "./pages/CheckoutPage3";
 
-import Autho from'./components/autho/Autho';
-import Account from './components/account/Account';
+import Autho from "./components/autho/Autho";
+import Account from "./components/account/Account";
 
-import Recovery from './components/recovery/Recovery';
+import Recovery from "./components/recovery/Recovery";
 import Choose from "./components/choose/Choose";
-
+import "./index.css";
 
 const App = () => {
   return (
@@ -26,11 +26,11 @@ const App = () => {
           <Route path="/checkout1" element={<CheckoutPage1 />} />
           <Route path="/checkout2" element={<CheckoutPage2 />} />
           <Route path="/checkout3" element={<CheckoutPage3 />} />
-           <Route path='/login' element={<Autho/>}/>
-      <Route path='/account' element={<Account/>}/>
-     <Route path='/Choose' element={<Choose/>}/>
+          <Route path="/login" element={<Autho />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/Choose" element={<Choose />} />
 
-<Route path='/Recovery' element={<Recovery/>}/>
+          <Route path="/Recovery" element={<Recovery />} />
         </Routes>
       </BrowserRouter>
       <Footer />
