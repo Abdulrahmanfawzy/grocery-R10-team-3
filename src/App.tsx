2545e17d0f09f20461d8f8fdbb54e216e1899b6d
Amr Mohamed Ali <amr.rashidy2020@gmail.com>
2026-03-05 05:19:59 +0200

    Resolve merge conflict: include all routes from dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
+import ProductList from "./pages/productList/ProductList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+
+import CheckoutPage1 from "./pages/CheckoutPage1";
+import CheckoutPage2 from "./pages/CheckoutPage2";
+import CheckoutPage3 from "./pages/CheckoutPage3";
+
+import Autho from './components/autho/Autho';
+import Account from './components/account/Account';
+
+import Recovery from './components/recovery/Recovery';
+import Choose from "./components/choose/Choose";
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Cart />} />
-        {/* <Route path="/checkout" element={<Checkout />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Cart />} />
+          <Route path="/shop" element={<ProductList />} />
+          <Route path="/checkout1" element={<CheckoutPage1 />} />
+          <Route path="/checkout2" element={<CheckoutPage2 />} />
+          <Route path="/checkout3" element={<CheckoutPage3 />} />
+          <Route path='/login' element={<Autho/>}/>
+          <Route path='/account' element={<Account/>}/>
+          <Route path='/Choose' element={<Choose/>}/>
+          <Route path='/Recovery' element={<Recovery/>}/>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 };
 
